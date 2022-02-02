bba2ec8adebda90b5b918d3ce0ea67a4f72968ba
Qin Guan <qinguan20040914@gmail.com>
2022-02-02 23:23:39 +0800

    Made some header changes

--- public/vue/components/header.js
+++ public/vue/components/header.js
@@ -2,12 +2,20 @@ export const Header = {
     template: `
         <div class="bg-slate-200 fixed top-0 px-3 h-14 w-screen flex items-center">
             <div class="container mx-auto">
-                <a href="/">
+                <a :href="href">
                     <span class="text-lg font-bold">{{ title ?? "Assignment" }}</span>
                 </a>
             </div>
         </div>
         <div class="mb-14"></div>
     `,
-    props: ['title']
+    props: ['title'],
+    computed: {
+        href: () => {
+            const h = window.location.href;
+            if (h.indexOf("https://ngeeannpoly.gitlab.io") > -1) return "https://ngeeannpoly.gitlab.io/fed/team2"
+            else if (h.indexOf("/Assignment/public") > -1) return "/Assignment/public"
+            return "/"
+        }
+    }
 }
