e3f9f77b0aa84529ea4124f9eaf7a99793bb0257
Qin Guan <qinguan20040914@gmail.com>
2022-02-03 20:51:09 +0800

    Updated credits

--- public/vue/pages/credits.js
+++ public/vue/pages/credits.js
@@ -1,20 +1,23 @@
-const links = [
-    "your references here",
-    "your references here",
-    "your references here",
-    "your references here",
-    "your references here",
-    "your references here",
-    "your references here",
-]
+const links = {
+    "Farrell": ["i am a reference", "i am a reference"],
+    "Qin Guan": ["i am a reference"],
+    "Yun-E": ["i am a reference"],
+    "Richard": ["i am a reference"],
+    "Ryan": ["i am a reference"],
+}
 
 export const Credits = {
     template: `
         <div class="w-screen h-screen flex flex-col">
             <v-header></v-header>
             <div class="container mx-auto flex-1">
-                <div class="p-3 border-b-2 border-slate-200" v-for="link in links">
-                    <a :href="link" class="link hover:underline text-slate-500 hover:text-slate-900">{{ link }}</a>
+                <div class="justify-start p-3 border-b-2 border-slate-200" v-for="(value, key) in links">
+                    <h1 class="text-2xl mb-3">{{ key }}</h1>
+                    <ul>
+                        <li v-for="link in value">
+                            <a :href="link" class="link hover:underline text-slate-500 hover:text-slate-900">{{ link }}</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <v-footer></v-footer>
