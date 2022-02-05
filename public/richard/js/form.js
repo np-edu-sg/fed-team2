250937ab8cf1969d6ae298f122e9b02a0dc0f0cc
SkyrusPreyas <skyrusrb@gmail.com>
2022-02-05 23:03:38 +0800

    Making Form :D

--- public/richard/js/form.js
+++ public/richard/js/form.js
@@ -1,3 +1,9 @@
 function toggleForm(){
     document.body.classList.toggle('activeForm');
+    document.getElementById("body").style.overflowY = "auto";
+}
+
+function goForm(){
+    document.getElementById("body").style.overflowY = "hidden";
+    window.scroll({top:0});
 }
