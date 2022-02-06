34db0330ed3d44847d1715785ed21728c67eee0d
Qin Guan <qinguan20040914@gmail.com>
2022-02-06 22:01:30 +0800

    Poggers

--- public/vue/components/header.js
+++ public/vue/components/header.js
@@ -1,4 +1,4 @@
-import {computed, ref} from "https://unpkg.com/vue@3.2.29/dist/vue.esm-browser.prod.js"
+import {computed, onBeforeUnmount, onMounted, ref} from "https://unpkg.com/vue@3.2.29/dist/vue.esm-browser.prod.js"
 
 export const Header = {
     //language=HTML
@@ -8,12 +8,12 @@ export const Header = {
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
               rel="stylesheet">
         <div :class="'fixed top-0 z-10 px-3 w-full shadow-md bg-slate-200 duration-200 font-body ' + (expanded ? 'h-96' : 'h-16')">
-            <div class="container mx-auto flex justify-between items-center mt-2">
+            <div class="container mx-auto flex justify-between items-center mt-3">
                 <a :href="href">
                     <span class="text-lg font-bold font-display hover:underline">{{ title ?? "Tourism@SG" }}</span>
                 </a>
 
-                <v-button color="alternative" @click="expanded = !expanded" class="bg-transparent mr-0 p-1!">
+                <v-button color="alternative" @click="expanded = !expanded" class="bg-transparent mr-0" v-if="sm">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24"
                          width="24px"
                          fill="#000000">
@@ -23,9 +23,15 @@ export const Header = {
                         <path v-else d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
                     </svg>
                 </v-button>
+
+                <div v-else>
+                    <v-button v-for="item in items" color="alternative" :href="item.href" class="bg-transparent">
+                        {{ item.title }}
+                    </v-button>
+                </div>
             </div>
 
-            <div v-if="expanded" class="container mx-auto overflow-hidden divide-y divide-slate-600">
+            <div v-if="expanded && sm" class="container mx-auto overflow-hidden divide-y divide-slate-600">
                 <div v-for="item in items" class="py-3">
                     <a :href="item.href" class="hover:underline">
                         {{ item.title }}
@@ -37,6 +43,7 @@ export const Header = {
     `,
     props: ['title'],
     setup() {
+        const width = ref(window.innerWidth)
         const expanded = ref(false)
         const href = computed(() => {
             const h = window.location.href;
@@ -46,9 +53,24 @@ export const Header = {
             return "/"
         })
 
+        const sm = computed(() => width.value <= 1280)
+        const update = () => {
+            width.value = window.innerWidth
+        }
+
+        onMounted(() => {
+            window.addEventListener("resize", update)
+        })
+
+        onBeforeUnmount(() => {
+            window.removeEventListener("resize", update)
+        })
+
         return {
+            sm,
+            width,
             href,
-            expanded
+            expanded,
         }
     },
     computed: {
